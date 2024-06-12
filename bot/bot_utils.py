8ba04e660c4e3f1d1d0820e60f96c846a060d39e
Khai Phan <khaiphn41@gmail.com>
2024-06-12 00:18:37 -0700

    Add bot util docstrings

--- bot/bot_utils.py
+++ bot/bot_utils.py
@@ -11,6 +11,17 @@ from bot.models import Guild, GetUsersStreamer
 
 
 def is_owner_or_optin_mode(engine: Engine):
+    """
+    Check if the author of a command is the owner of the guild or if the guild's notification mode is 'optin'.
+    Used as a decorator for checking permissions of command handlers
+
+    Parameters:
+    - engine (Engine): The SQLAlchemy engine to use for database operations.
+    - ctx (Context): The context of the command being invoked.
+
+    Returns:
+    - bool: True if the author is the guild owner or the guild's notification mode is 'optin', False otherwise.
+    """
     def predicate(ctx: Context) -> bool:
         with Session(engine) as session:
             guild_notif_mode = session.scalar(
@@ -20,6 +31,17 @@ def is_owner_or_optin_mode(engine: Engine):
 
 
 def is_owner(interaction: discord.Interaction) -> bool:
+    """
+    Check if the author of an interaction is the owner of the guild.
+    Used as a decorator for checking permissions of command handlers
+
+    Parameters:
+    - interaction (discord.Interaction): The interaction object representing the command being invoked.
+
+    Returns:
+    - bool: True if the author of the interaction is the guild owner, False otherwise.
+    """
+
     if interaction.guild is None or interaction.user is None or interaction.guild.owner is None:
         return False
     user_is_owner = interaction.user.id == interaction.guild.owner.id
@@ -29,18 +51,30 @@ def is_owner(interaction: discord.Interaction) -> bool:
 
 def get_first_sendable_text_channel(guild: discord.Guild) -> Optional[discord.TextChannel]:
     """
-    Get the first text channel in the guild that the bot has permission to send messages to.
+    Returns the first text channel in the guild that the bot has permission to send messages to.
 
-    Args:
-        guild (discord.Guild): The guild to search for the text channel in.
+    Parameters:
+    - guild (discord.Guild): The guild to search for a sendable text channel.
 
     Returns:
-        discord.TextChannel | None: The first text channel that the bot can send messages to, or None if no such channel exists.
+    - Optional[discord.TextChannel]: The first text channel in the guild that the bot can send messages to, or None if no such channel is found.
     """
+
     return next((channel for channel in guild.text_channels if channel.permissions_for(guild.me).send_messages), None)
 
 
 async def streamer_get_ids_names_from_logins(twitch: Twitch, broadcaster_logins: list[str]) -> list[GetUsersStreamer]:
+    """
+    Get a list of GetUsersStreamer objects by providing a list of broadcaster logins.
+
+    Parameters:
+    - twitch (Twitch): An instance of the Twitch class.
+    - broadcaster_logins (list[str]): A list of broadcaster logins for which to retrieve user information.
+
+    Returns:
+    - list[GetUsersStreamer]: A list of GetUsersStreamer objects containing the id and name of each broadcaster.
+    """
+
     try:
         return [GetUsersStreamer(user.id, user.display_name) async for user in twitch.get_users(logins=broadcaster_logins)]
     except TwitchAPIException as e:
@@ -49,6 +83,17 @@ async def streamer_get_ids_names_from_logins(twitch: Twitch, broadcaster_logins:
 
 
 async def validate_streamer_ids_get_names(twitch: Twitch, ids: list[str]) -> list[GetUsersStreamer]:
+    """
+    Validate the given list of user ids by fetching their display names from the Twitch API.
+
+    Parameters:
+    - twitch (Twitch): An instance of the Twitch class used to make API calls.
+    - ids (list[str]): A list of user ids to validate.
+
+    Returns:
+    - list[GetUsersStreamer]: A list of GetUsersStreamer objects containing the id and display name of each user.
+    """
+
     try:
         return [GetUsersStreamer(user.id, user.display_name) async for user in twitch.get_users(user_ids=ids)]
     except TwitchAPIException as e:
